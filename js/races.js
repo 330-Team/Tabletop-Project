03dff01c60420e02fa65b4437e96d1026bb506e3
Andrew Bridgeman <andrewbridgeman@dhcp-10-105-32-240.wireless.northwestern.private>
2020-03-02 20:32:53 -0600

    multiple users

--- js/races.js
+++ js/races.js
@@ -59,12 +59,14 @@ const race_click = (ev_id) =>{
 }
 
 const add_to_pad = () =>{
+  let user = localStorage.getItem("curr");
+
   localStorage.setItem("track-race", "1");
   if(localStorage.getItem("done") != "1" && localStorage.getItem("track-class") == "1" && localStorage.getItem("track-weapon") == "1" && localStorage.getItem("track-spell") == "1"){
     alert("Character creation achievement earned! \n You have created your very first character. See your notepad for more details.");
     localStorage.setItem("done", "1");
   }
-  localStorage.setItem("race", current_data);
+  localStorage.setItem(user + "race", current_data);
   alert("Added to notepad");
 }
 
