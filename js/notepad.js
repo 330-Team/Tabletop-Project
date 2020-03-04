3feb8756f004e1e9c956789f202fb407d20e0762
brndnwrd <brndnwrd@gmail.com>
2020-03-03 21:58:26 -0600

    materialized notepad css

--- js/notepad.js
+++ js/notepad.js
@@ -1,7 +1,23 @@
 window.onload = function() {
   let user = localStorage.getItem("curr");
-  document.getElementById("race").innerHTML = "Race".bold().fontsize(6) + localStorage.getItem(user + "race");
-  document.getElementById("class").innerHTML += "Class".bold().fontsize(6) + localStorage.getItem(user + "class");
-  document.getElementById("weapon").innerHTML += "Weapon".bold().fontsize(6) + localStorage.getItem(user + "weapon");
-  document.getElementById("spell").innerHTML += "Spell".bold().fontsize(6) + localStorage.getItem(user + "spell");
+
+  document.getElementById("race").innerHTML = "Race".bold().fontsize(6)
+  if (localStorage.getItem(user + "race")) {
+    document.getElementById("race").innerHTML += localStorage.getItem(user + "race");
+  }
+
+  document.getElementById("class").innerHTML = "Class".bold().fontsize(6)
+  if (localStorage.getItem(user + "class")) {
+    document.getElementById("class").innerHTML += localStorage.getItem(user + "class");
+  }
+
+  document.getElementById("weapon").innerHTML = "Weapon".bold().fontsize(6)
+  if (localStorage.getItem(user + "weapon")) {  
+    document.getElementById("weapon").innerHTML += localStorage.getItem(user + "weapon");
+  }
+
+  document.getElementById("spell").innerHTML = "Spell".bold().fontsize(6)
+  if (localStorage.getItem(user + "spell")) {
+    document.getElementById("spell").innerHTML += localStorage.getItem(user + "spell");
+  }
 }
