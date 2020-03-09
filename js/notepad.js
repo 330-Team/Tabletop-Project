bc2555e125e1b67255fa8d1c72193506395c9864
brndnwrd <brndnwrd@gmail.com>
2020-03-09 00:53:00 -0500

    framework and first section of userpage

--- js/notepad.js
+++ js/notepad.js
@@ -3,22 +3,22 @@ window.onload = function() {
 
   document.getElementById("username").innerHTML += localStorage.getItem("curr");
 
-  document.getElementById("race").innerHTML = "Race".bold().fontsize(6)
+  document.getElementById("race").innerHTML = `<font size="6" class="blue-grey-text text-lighten-4"><b>Race</b></font>`
   if (localStorage.getItem(user + "race")) {
     document.getElementById("race").innerHTML += localStorage.getItem(user + "race");
   }
 
-  document.getElementById("class").innerHTML = "Class".bold().fontsize(6)
+  document.getElementById("class").innerHTML = `<font size="6" class="blue-grey-text text-lighten-4"><b>Class</b></font>`
   if (localStorage.getItem(user + "class")) {
     document.getElementById("class").innerHTML += localStorage.getItem(user + "class");
   }
 
-  document.getElementById("weapon").innerHTML = "Weapon".bold().fontsize(6)
+  document.getElementById("weapon").innerHTML = `<font size="6" class="blue-grey-text text-lighten-4"><b>Weapon</b></font>`
   if (localStorage.getItem(user + "weapon")) {
     document.getElementById("weapon").innerHTML += localStorage.getItem(user + "weapon");
   }
 
-  document.getElementById("spell").innerHTML = "Spell".bold().fontsize(6)
+  document.getElementById("spell").innerHTML = `<font size="6" class="blue-grey-text text-lighten-4"><b>Spell</b></font>`
   if (localStorage.getItem(user + "spell")) {
     document.getElementById("spell").innerHTML += localStorage.getItem(user + "spell");
   }
--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,23 @@
-const signOut = () =>{
-    window.location.href = "signup.html";
+window.onload = function() {
+    document.getElementById("username").innerHTML += localStorage.getItem("curr");
+}
+
+document.getElementById("userbox").innerHTML = `<font size="6" class="blue-grey-text text-lighten-4"><b>${localStorage.getItem("curr")}</b></font>`
+
+const user = localStorage.getItem("curr");
+
+if (localStorage.getItem(user + "race")) {
+    document.getElementById("raceCheck").className = "far fa-check-square"
+}
+
+if (localStorage.getItem(user + "class")) {
+    document.getElementById("classCheck").className = "far fa-check-square"
+}
+
+if (localStorage.getItem(user + "weapon")) {
+    document.getElementById("weaponCheck").className = "far fa-check-square"
+}
+
+if (localStorage.getItem(user + "spell")) {
+    document.getElementById("spellCheck").className = "far fa-check-square"
 }
